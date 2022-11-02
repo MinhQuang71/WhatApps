c2eac0ef2f06911ec55ef3e3bb14b4071e31e9e6
Rachelli.x <username@example.com>
2022-11-02 23:24:53 +0700

    UI-4 Chats List

---
+++ src/components/screens/ChatsScreen.js
@@ -0,0 +1,13 @@
+import { FlatList } from 'react-native';
+import ChatListItem from '../ChatListItem';
+import chats from '../../../assets/data/chats.json';
+const ChatsScreen = () => {
+    return (
+        <FlatList
+          data={chats}
+          renderItem={({ item }) => <ChatListItem chat={item} />}
+        />
+      );
+};
+
+export default ChatsScreen;
